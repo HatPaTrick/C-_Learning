2713b828815e9a3f4d98c0d659326b78ffc5626f
Patrick Ramos <135259062+HatPaTrick@users.noreply.github.com>
2024-02-07 19:03:05 -0300

    making some changes

--- ScreenSound/ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/ScreenSound/obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306b74e466931640b859b0d3dd196e1704b15374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1779918d7c7eeaad61d3fb8b5bb670f86b350ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
