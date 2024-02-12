28c78b8f3eb0f00f8f84aa553ca938e5427f4480
Patrick Ramos <135259062+HatPaTrick@users.noreply.github.com>
2024-02-12 10:32:17 -0300

    Commiting Changes

---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,18 @@
+﻿Banda queen = new Banda("Queen");
+Album albumDoQueen = new Album("A night at the opera");
+
+
+Musica musica1 = new Musica(queen);
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
+
+Musica musica2 = new Musica(queen);
+musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
+
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
---
+++ ScreenSound/ScreenSound/musica.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+class Musica
+{
+    public Musica(Banda artista)
+    {
+        Artista = artista;
+    }
+
+    //Flamengo
+    public string Nome { get; set; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+    public Genero Genero { get; set; }
+    public string DescricaoResumida => 
+        $"A música {Nome} pertence à banda {Artista}";
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        Console.WriteLine($"Descrição: {DescricaoResumida}");
+        if (Disponivel)
+
+
+        {
+            Console.WriteLine("Música disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Premium.");
+        }
+
+    }
+}
