2a48c50f80accf77012a562af8afd29f097ec825
Patrick Ramos <135259062+HatPaTrick@users.noreply.github.com>
2024-02-22 11:42:28 -0300

    Adding Files

---
+++ desafios_alura/desafios/Challenger/Challenger/Artista.cs
@@ -0,0 +1,39 @@
+﻿namespace Alura.Filmes;
+
+public class Artista
+{
+    public Artista(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+        FilmesAtuados = new List<Filme>();
+    }
+
+    public string Nome { get; }
+    public int Idade { get; }
+    public List<Filme> FilmesAtuados { get; set; }
+    public int QuantidadeDeFilmes => FilmesAtuados.Count;
+
+
+    public void AdicionarFilme(Filme filme) 
+    {
+        FilmesAtuados.Add(filme);
+        if (!filme.Elenco.Contains(this)) filme.AdicionarElenco(this);
+    }
+
+    public void MostrarFilmesAtuados()
+    {
+        if(QuantidadeDeFilmes == 0)
+        {
+            Console.WriteLine($"Nenhum filme encontrado na base de dados para {this.Nome}");
+            return;
+        }
+
+        foreach (var filme in FilmesAtuados)
+        {
+            Console.WriteLine("Filmes: ");
+            Console.WriteLine(filme);
+        }
+    }
+
+}
---
+++ desafios_alura/desafios/Challenger/Challenger/Program.cs
@@ -0,0 +1,18 @@
+﻿using Alura.Filmes;
+
+Filme filme = new Filme("Um sonho da liberdade", 142, new List<string>() {"Tim Robbins", "Morgan Freeman"});
+Filme filme2 = new("O poderoso chefão", 175, new List<string>() { "Marlon Brando", "Al Pacino", "Talia Shire" });
+Filme filme3 = new("Batman - O Cavaleiro das Trevas", 152, new List<string>() { "Christian Bale", "Heath Ledger", "Maggie Gyleenhaal" });
+Filme filme4 = new("Senhor dos Anéis - O Retorno do Rei", 201, new List<string>() { "Elijah Wood", "Ian McKellen", "Viggo Mortensen" });
+Filme filme5 = new("Green Book - O Guia", 130, new List<string>() { "Viggo Mortensen", "Mahershala Ali" });
+
+
+List<Filme> meusFilmesFavoritos = new List<Filme>() { filme, filme2, filme3, filme4, filme5 };
+
+foreach (Filme f in meusFilmesFavoritos)
+{
+    Console.WriteLine($"Filme: {f.Titulo}");
+    Console.WriteLine($"Duração: {f.Duracao}");
+    f.ListarElenco();
+    Console.WriteLine();
+}
