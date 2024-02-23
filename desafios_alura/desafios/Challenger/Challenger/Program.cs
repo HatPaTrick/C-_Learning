8e5a60de9956eb0ad76246c6fd85da71b4c4fc73
Patrick Ramos <135259062+HatPaTrick@users.noreply.github.com>
2024-02-23 16:05:55 -0300

    edit

--- desafios_alura/desafios/Challenger/Challenger/Program.cs
+++ desafios_alura/desafios/Challenger/Challenger/Program.cs
@@ -1,18 +1,47 @@
-﻿using Alura.Filmes;
-
-Filme filme = new Filme("Um sonho da liberdade", 142, new List<string>() {"Tim Robbins", "Morgan Freeman"});
-Filme filme2 = new("O poderoso chefão", 175, new List<string>() { "Marlon Brando", "Al Pacino", "Talia Shire" });
-Filme filme3 = new("Batman - O Cavaleiro das Trevas", 152, new List<string>() { "Christian Bale", "Heath Ledger", "Maggie Gyleenhaal" });
-Filme filme4 = new("Senhor dos Anéis - O Retorno do Rei", 201, new List<string>() { "Elijah Wood", "Ian McKellen", "Viggo Mortensen" });
-Filme filme5 = new("Green Book - O Guia", 130, new List<string>() { "Viggo Mortensen", "Mahershala Ali" });
-
-
-List<Filme> meusFilmesFavoritos = new List<Filme>() { filme, filme2, filme3, filme4, filme5 };
-
-foreach (Filme f in meusFilmesFavoritos)
+﻿
+class Pet
 {
-    Console.WriteLine($"Filme: {f.Titulo}");
-    Console.WriteLine($"Duração: {f.Duracao}");
-    f.ListarElenco();
-    Console.WriteLine();
+    public Pet(string nome, int idade, string especie)
+    {
+        Nome = nome;
+        Idade = idade;
+        Especie = especie;
+    }
+
+    public string Nome { get; }
+    public int Idade { get; }
+    public string Especie { get; }
+}
+
+class Dono
+{
+    public Dono(string nome, string contato)
+    {
+        Nome = nome;
+        Contato = contato;
+    }
+
+    public string Nome { get; }
+    public string Contato { get; }
+
+
+}
+
+class Consulta
+{
+    public Pet Animal { get; }
+    public Dono Dono { get; }
+}
+
+class Medico
+{
+    public Medico(string nome, string especialidade)
+    {
+        Nome = nome;
+        Especialidade = especialidade;
+    }
+
+    public string Nome { get; }
+    public string Especialidade { get; }
+
 }
