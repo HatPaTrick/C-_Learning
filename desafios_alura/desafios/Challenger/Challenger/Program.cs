deaeffa97200c9934d8a4a9e42f952f6e7a89eb4
Patrick Ramos <135259062+HatPaTrick@users.noreply.github.com>
2024-03-05 11:41:11 -0300

    Edit

--- desafios_alura/desafios/Challenger/Challenger/Program.cs
+++ desafios_alura/desafios/Challenger/Challenger/Program.cs
@@ -1,36 +1,83 @@
-﻿public class Calculadora
+﻿namespace Program;
+
+/*abstract class FormaGeometrica
 {
-    public static double Calcular(double numero1, double numero2, char operacao)
+    public abstract double CalcularArea();
+    public abstract double CalcularPerimetro();
+}
+
+
+
+
+class Trigangulo : FormaGeometrica
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+
+    public override double CalcularArea()
     {
-        double resultado = 0;
-
-        switch (operacao)
-        {
-            case '+':
-                resultado = Somar(numero1 + numero2);
-                break;
-            case '-':
-                resultado = Subtrair(numero1 - numero2);
-                break;
-            case '*':
-                resultado = Multiplicar(numero1 * numero2);
-                break;
-            case '/':
-                resultado = Dividir(numero1 / numero2);
-                break;
-            case '%':
-                resultado = Porcentagem(numero1, numero2);
-                break;
-            case '^':
-                resultado = Potencia(numero1, numero2);
-                break;
-            case 'r':
-                resultado = RaizQuadrada(numero1);
-                break;
-            default:
-                resultado = 0;
-                break;
-
-        }
+        return 0.5 * A * C;
     }
+
+    public override double CalcularPerimetro()
+    {
+        return A + B + C + Math.Sqrt((A - B) * (A - B) + (B - C) * (B - C));
+    }
+}
+
+
+class Quadrado : FormaGeometrica
+{
+   public double Lado { get; set; }
+
+    public override double CalcularArea()
+    {
+        return Lado * Lado;
+    }
+
+    public override double CalcularPerimetro()
+    {
+        return 4 * Lado;
+    }
+}
+
+class Circulo : FormaGeometrica
+{
+    public double R { get; set; }
+
+    public override double CalcularArea()
+    {
+        return Math.PI * R * R;
+    }
+
+    public override double CalcularPerimetro()
+    {
+        return 2 * Math.PI * R;
+    }
+}
+
+*/
+
+class Funcionarios
+{
+    public string Nome { get; set; }
+    public string Funcao { get; set; }
+    public double Salario { get; set; }
+}
+
+class Gerente : Funcionarios
+{
+    public string Setor { get; set; }
+}
+
+class Programador : Funcionarios
+{
+    public string LinguagemDeProgramacao { get; set; } // C#, C++, Jav
+    public string Framework { get; set; }
+}
+
+class Analista : Funcionarios
+{
+    public string AreaDeAtuacao { get; set; }
 }
