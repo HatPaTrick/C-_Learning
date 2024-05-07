f0558e6bab73e8d951ceaa647d82bbfc4779f1eb
Patrick Ramos <135259062+HatPaTrick@users.noreply.github.com>
2024-05-07 18:43:47 -0300

    edit

--- ScreenSound-04/desafios_alura/Modulo_1/desafios_alura/obj/Debug/net8.0/desafios_alura.AssemblyInfo.cs
+++ ScreenSound-04/desafios_alura/Modulo_1/desafios_alura/obj/Debug/net8.0/desafios_alura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desafios_alura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0527bc404b753d7b044b0c3b108092041754ff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea09dcb2755ae318f99f32356a90b9df72fa206")]
 [assembly: System.Reflection.AssemblyProductAttribute("desafios_alura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desafios_alura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
