15508d6fef8f1756f6dec8a4c96296fdd067f71d
Patrick Ramos <135259062+HatPaTrick@users.noreply.github.com>
2024-06-19 19:58:16 -0300

    Flamengo

--- ScreenSound-04/ScreenSound-04/ScreenSound-04/Program.cs
+++ ScreenSound-04/ScreenSound-04/ScreenSound-04/Program.cs
@@ -1,11 +1,4 @@
 ﻿using (HttpClient client = new HttpClient())
 {
-    try
-    {
-        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Tivemos um problema: {e.Message}");
-    }
+    client.GetStringAsync
 }
